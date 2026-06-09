9214bccf295eb965ce47fb3ba18dba099ed1b35a
mivancic <mivancic@aerosolmageesci.com>
2026-06-09 08:15:20 +0200

    small update for SerialNumber columns.

--- TCP-IP_AE33_first_example_2026-06-08.py
+++ TCP-IP_AE33_first_example_2026-06-08.py
@@ -16,9 +16,9 @@ RECV_TIMEOUT = 2.0  # seconds
 
 COLUMNS_AE33_DATA = ['SerialNumber', 'ID', 'StartTime', 'EndTime', 'SetupID', 'SetupTimestamp', 'Ref1', 'Sens11', 'Sens12', 'Ref2', 'Sens21', 'Sens22', 'Ref3', 'Sens31', 'Sens32', 'Ref4', 'Sens41', 'Sens42', 'Ref5', 'Sens51', 'Sens52', 'Ref6', 'Sens61', 'Sens62', 'Ref7', 'Sens71', 'Sens72', 'BC11', 'BC12', 'BC1', 'BC21', 'BC22', 'BC2', 'BC31', 'BC32', 'BC3', 'BC41', 'BC42', 'BC4', 'BC51', 'BC52', 'BC5', 'BC61', 'BC62', 'BC6', 'BC71', 'BC72', 'BC7', 'K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7', 'BB', 'Pressure', 'Temp', 'Flow1', 'Flow2', 'FlowC', 'T_controller', 'T_supply', 'T_LED', 'ControllerStatus', 'LEDStatus', 'DetectorStatus', 'ValveStatus', 'Status', 'TapeAdvanceCount', 'TapeAdvanceLeft', 'CPU', 'DiskSpace', 'NumConnections']
 
-COLUMNS_AE33_EXTERNAL_DEVICE_DATA = ['ID', 'DataID', 'DeviceID', 'DeviceData']
+COLUMNS_AE33_EXTERNAL_DEVICE_DATA = ['SerialNumber', 'ID', 'DataID', 'DeviceID', 'DeviceData']
 
-COLUMNS_AE33_SETUP = ['ID', 'SerialNumber', 'Timestamp', 'FirmwareVer', 'SoftwareVer', 'DataCenterIP', 'AutoConnect', 'InletFilter', 'Timebase', 'SG1', 'SG2', 'SG3', 'SG4', 'SG5', 'SG6', 'SG7', 'C', 'Area', 'Zeta', 'Aff', 'Abb', 'Pressure', 'Temp', 'ATNf1', 'ATNf2', 'Kmax', 'Kmin', 'Flow', 'FlowRepStd', 'PumpPresetValue', 'FlowFormulaA0', 'FlowFormulaA1', 'FlowFormulaB0', 'FlowFormulaB1', 'FlowFormulaC0', 'FlowFormulaC1', 'FlowFormulaD', 'FlowFormulaE', 'FlowFormulaF', 'TAtype', 'TAatnMax', 'TAinterval', 'TAtime', 'TapeRightFormulaK', 'TapeRightFormulaN', 'TapeLeftFormulaK', 'TapeLeftFormulaN', 'WarmUpInterval', 'AutoTestEnabled', 'AutoTestType', 'AutoTestDay', 'AutoTestTime', 'MeasureTimeStamp', 'HomeInfo', 'Display', 'About', 'DST', 'TimeZone', 'TapeAdvanceAdjust', 'ExternalID', 'BHparamID', 'TimeSync', 'DHCP', 'InstrumentIP', 'SubnetMask', 'Gateway', 'Baud', 'NTPserver']
+COLUMNS_AE33_SETUP = ['serial', 'ID', 'SerialNumber', 'Timestamp', 'FirmwareVer', 'SoftwareVer', 'DataCenterIP', 'AutoConnect', 'InletFilter', 'Timebase', 'SG1', 'SG2', 'SG3', 'SG4', 'SG5', 'SG6', 'SG7', 'C', 'Area', 'Zeta', 'Aff', 'Abb', 'Pressure', 'Temp', 'ATNf1', 'ATNf2', 'Kmax', 'Kmin', 'Flow', 'FlowRepStd', 'PumpPresetValue', 'FlowFormulaA0', 'FlowFormulaA1', 'FlowFormulaB0', 'FlowFormulaB1', 'FlowFormulaC0', 'FlowFormulaC1', 'FlowFormulaD', 'FlowFormulaE', 'FlowFormulaF', 'TAtype', 'TAatnMax', 'TAinterval', 'TAtime', 'TapeRightFormulaK', 'TapeRightFormulaN', 'TapeLeftFormulaK', 'TapeLeftFormulaN', 'WarmUpInterval', 'AutoTestEnabled', 'AutoTestType', 'AutoTestDay', 'AutoTestTime', 'MeasureTimeStamp', 'HomeInfo', 'Display', 'About', 'DST', 'TimeZone', 'TapeAdvanceAdjust', 'ExternalID', 'BHparamID', 'TimeSync', 'DHCP', 'InstrumentIP', 'SubnetMask', 'Gateway', 'Baud', 'NTPserver']
 # %%
 def receive_all(sock, buffer_size=4096, timeout=2.0):
     sock.settimeout(timeout)
@@ -105,6 +105,8 @@ print(text)
 df_ae33_setup = pd.read_csv(StringIO(text.replace('AE33>', '').strip()),
                             sep='|',
                             names=COLUMNS_AE33_SETUP)
+# drop duplicated serial column from setup data
+df_ae33_setup = df_ae33_setup.drop(columns=['serial'], errors='ignore')
 print()
 print(df_ae33_setup)
 # %%
